b7504da593ce2f1a706788daf0265e06754b8ef7
ErronLLP <48717807+ErronLLP@users.noreply.github.com>
2022-10-15 22:07:34 +1100

    final

--- VisTwoFile/VIShomework2.js
+++ VisTwoFile/VIShomework2.js
@@ -1,11 +1,17 @@
 var vg_1 = "VISindex.vg.json"
-vegaEmbed("#graph_chart", vg_1) .then(function(result) {
+vegaEmbed("#graph_chart", vg_1, { "actions": false }) .then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 
 }) .catch(console.error);
 
 var vg_2 = "VISindex2.vg.json"
-vegaEmbed("#bar_chart", vg_2) .then(function(result) {
+vegaEmbed("#bar_chart", vg_2, { "actions": false }) .then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+
+}) .catch(console.error);
+
+var vg_3 = "VISindex3.vg.json"
+vegaEmbed("#pie_chart", vg_3, { "actions": false }) .then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 
 }) .catch(console.error);
